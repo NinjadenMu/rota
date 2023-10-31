7b4773b89e9f66bbd1d5cc8d4f595457a01ab236
NinjadenMu <jaden.mu@gmail.com>
2023-10-31 00:53:40 -0400

    Update game.py

--- game.py
+++ game.py
@@ -26,13 +26,12 @@ def get_legal_moves(to_move, pieces):
     all_tiles_placed = True
 
     for i, start in enumerate(player_pieces):
-        if all_tiles_placed:
+        if all_tiles_placed and start == -1:
             all_tiles_placed = False
-            
-            if start == -1:
-                for stop in range(NUM_SPOTS):
-                    if stop not in pieces[0] and stop not in pieces[1]:
-                        moves.append((start, stop))
+
+            for stop in range(NUM_SPOTS):
+                if stop not in pieces[0] and stop not in pieces[1]:
+                    moves.append((start, stop))
 
         else:
             pseudolegal_moves = ADJACENCY_MATRIX[start]
@@ -64,11 +63,13 @@ def make_move(to_move, pieces, move):
 def undo_move(to_move, pieces, move):
     pieces[to_move][pieces[to_move].index(move[1])] = move[0]
 
-def search(to_move, pieces, moves, depth):
+def search(to_move, pieces, moves, depth, alpha, beta):
     result = check_for_win(pieces)
     if result:
-        #print(pieces)
-        return result
+        if result < 0:
+            return result - (depth / (depth + 2))
+
+        return result + (depth / (depth + 2))
 
     if depth == 0:
         return result
@@ -76,23 +77,56 @@ def search(to_move, pieces, moves, depth):
     if not len(moves):
         return result
 
-    results = []
+    best_result = -3
+    worst_result = 3
     for move in moves:
         make_move(to_move, pieces, move)
         to_move = not to_move
 
         opp_moves = get_legal_moves(to_move, pieces)
-        results.append(search(to_move, pieces, opp_moves, depth - 1))
+        result = search(to_move, pieces, opp_moves, depth - 1, alpha, beta)
+        best_result = max(result, best_result)
+        worst_result = min(result, worst_result)
 
         to_move = not to_move
         undo_move(to_move, pieces, move)
 
-    if to_move:
-        return min(results)
+        if not to_move:
+            alpha = max(alpha, best_result)
+            if beta <= alpha:
+                break
 
-    return max(results)
+        else:
+            beta = min(beta, worst_result)
+            if beta <= alpha:
+                break
 
-print(search(to_move, pieces, get_legal_moves(to_move, pieces), 4))
+    if not to_move:
+        return best_result
+
+    return worst_result
+
+#print(search(to_move, pieces, get_legal_moves(to_move, pieces), 8, -1, 1))
+while not check_for_win(pieces):
+    print(pieces)
+
+    moves = get_legal_moves(to_move, pieces)
+    results = []
+
+    for move in moves:
+        results.append(search(to_move, pieces, [move], 7, -1, 1))
+
+    if not to_move:
+        make_move(to_move, pieces, moves[results.index(max(results))])
+
+    else:
+        make_move(to_move, pieces, moves[results.index(min(results))])
+
+    to_move = not to_move
+
+
+print(pieces)
+print(check_for_win(pieces))
 
 while not check_for_win(pieces):
     print(pieces)
